33d145855dd37feb06a9e78811823aa92dc11adb
Saad Zulfiqar <saadabbasi4711@gmail.com>
2025-07-03 04:03:21 +0500

    Initiated auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
