69b983b2fea8edb5ca0d9e301df4f1f35e9273b3
Saad Zulfiqar <saadabbasi4711@gmail.com>
2025-07-03 03:54:39 +0500

    User CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
